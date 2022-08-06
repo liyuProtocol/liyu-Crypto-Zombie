d9f11327c1e8ef999f695e32432c29cb05c14c32
liyuwangluo <liyu2016520@163.com>
2022-08-06 11:00:40 +0800

    Updates

---
+++ precache-manifest.72f4354d22d5611b2a6e5fa254b0e542.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ab8061fa7fe5e39835223c1a1508ec2",
+    "url": "/liyu-Crypto-Zombie/index.html"
+  },
+  {
+    "revision": "d79f3cb7360db643f9c0",
+    "url": "/liyu-Crypto-Zombie/static/css/main.74d05a19.chunk.css"
+  },
+  {
+    "revision": "db8573508d7fcaf12aa4",
+    "url": "/liyu-Crypto-Zombie/static/js/2.b334f2d2.chunk.js"
+  },
+  {
+    "revision": "f359eeb3e45f92b30cd84295682b7585",
+    "url": "/liyu-Crypto-Zombie/static/js/2.b334f2d2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d79f3cb7360db643f9c0",
+    "url": "/liyu-Crypto-Zombie/static/js/main.e647d968.chunk.js"
+  },
+  {
+    "revision": "8c96c0b3e438ce4265a9",
+    "url": "/liyu-Crypto-Zombie/static/js/runtime-main.3359feef.js"
+  },
+  {
+    "revision": "d2b2559b651e6a50f8d43f206dcdb8e7",
+    "url": "/liyu-Crypto-Zombie/static/media/Btn_Register_Normal.d2b2559b.png"
+  },
+  {
+    "revision": "d3684ace4f10c62c9209b45d3bb724c2",
+    "url": "/liyu-Crypto-Zombie/static/media/Btn_Register_Over.d3684ace.png"
+  },
+  {
+    "revision": "64a6f16c4f5ef7e189e75dfc0a52a25f",
+    "url": "/liyu-Crypto-Zombie/static/media/homecard@2x.64a6f16c.png"
+  },
+  {
+    "revision": "9d0437bbb5e705bad95ac929361faf3e",
+    "url": "/liyu-Crypto-Zombie/static/media/index_bg.9d0437bb.png"
+  },
+  {
+    "revision": "41236031fc933b053717ef1760601e36",
+    "url": "/liyu-Crypto-Zombie/static/media/start_btn.41236031.png"
+  },
+  {
+    "revision": "2a4a2235f6ed0e838b49a3f1bec4aae1",
+    "url": "/liyu-Crypto-Zombie/static/media/tester-bg@2x.2a4a2235.png"
+  },
+  {
+    "revision": "b102b8003aab95865cb7363428273a16",
+    "url": "/liyu-Crypto-Zombie/static/media/walls.b102b800.jpg"
+  },
+  {
+    "revision": "08087132d6dcfcd5c5f471880c104712",
+    "url": "/liyu-Crypto-Zombie/static/media/zombierun.08087132.png"
+  }
+]);
